102c9c0ce0f30d2650ff66662d4e84048f18f2f2
Nato Wickersom <nattobean@Natos-MacBook-Air.local>
2024-08-18 21:15:25 +1200

    Bits added

--- textbook/chapter2/bits_learning.c
+++ textbook/chapter2/bits_learning.c
@@ -1,7 +1,8 @@
 #include <stdio.h> 
 
-void print_bits16(unsigned short int x) {
-    print("%d bit format: ", x);
+
+void print_bits16(unsigned short int num) {
+    printf("%d bit format: ", num);
     for (int i = 15; i >= 0; i--) {
         unsigned short mask = 1 << i;
         unsigned short bit = (num & mask) ? 1 : 0;
@@ -10,16 +11,31 @@ void print_bits16(unsigned short int x) {
     printf("\n");
 }
 
+unsigned int get_bits_right(int x, int p, int n) /* get n bits from position p */
+{
+    print_bits16(~0 << n);
+    print_bits16((1U << (16-n)) - 1);
+    return((x >> (p+1-n)) &  ~(~0 << n));
+}
 
-unsigned int get_bits(int x, int p, int n) {
-    return (( x << (p+1-n)) );
+unsigned int get_bits_left(int x, int p, int n)
+{
+    return ((x << (p+1-n)));
 }
 
 
 int main() {
 
-    unsigned short int x = 100; 
-    printf("%u\n", x);
+    unsigned short int x = 65535; 
+    // print_bits16(x);
+    int y = get_bits_right(x, 5, 4);
+    int z = get_bits_left(x, 3, 8);
+    // print_bits16(y);
+
+    unsigned int b = 0b1111111;
+    // print_bits16(b);
+    b = b >> 5;
+    // print_bits16(b);
 
     return 1;
 }
