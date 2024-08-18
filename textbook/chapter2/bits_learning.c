74acc509a3bdc929b7a49f61a4d7bf05bb197620
Nato Wickersom <nattobean@Natos-MacBook-Air.local>
2024-08-18 22:10:18 +1200

    Rotate bits to right

--- textbook/chapter2/bits_learning.c
+++ textbook/chapter2/bits_learning.c
@@ -1,8 +1,31 @@
 #include <stdio.h> 
 
 
+// unsigned int number_length_calculator(int x) {
+//     unsigned int cnt = 0;
+//     while(x += 0) {
+//         x <<= 1;
+//         cnt++;
+//     }
+//     return cnt;
+// }
+
+
+// void print_bits(int num) {
+//     printf("%d bit format: ", num);
+    
+//     for (int i = number_length_calculator(num); i >= 0; i--) {
+//         unsigned short mask = 1 << i;
+//         unsigned short bit = (num & mask) ? 1 : 0;
+//         printf("%u", bit);
+//     }
+//     printf("\n");
+// }
+
+
 void print_bits16(unsigned short int num) {
     printf("%d bit format: ", num);
+    
     for (int i = 15; i >= 0; i--) {
         unsigned short mask = 1 << i;
         unsigned short bit = (num & mask) ? 1 : 0;
@@ -13,29 +36,57 @@ void print_bits16(unsigned short int num) {
 
 unsigned int get_bits_right(int x, int p, int n) /* get n bits from position p */
 {
-    print_bits16(~0 << n);
-    print_bits16((1U << (16-n)) - 1);
-    return((x >> (p+1-n)) &  ~(~0 << n));
+    return((x >> (p+1-n)) & ~(~0 << n));
 }
 
 unsigned int get_bits_left(int x, int p, int n)
 {
-    return ((x << (p+1-n)));
+    unsigned short int zero_complement = ~0;
+    return ((x << (p+1-n)) & ~(zero_complement >> n));
 }
 
+unsigned int word_length_calculator() {
+    unsigned int x = 1;
+    unsigned int cnt = 0;
+    while(x += 0) {
+        x <<= 1;
+        cnt++;
+    }
+    return cnt;
+}
+
+unsigned int rotate_right(unsigned short int x, int b) {
+    unsigned int y = x << (16 - b);
+    return (x >> b) | y;
+}
 
 int main() {
+    printf("----------------------------------------------\n");
 
-    unsigned short int x = 65535; 
-    // print_bits16(x);
+    printf("Modified get_bits to move from left to right\n");
+    unsigned short int x = ~0; 
+    print_bits16(x);
     int y = get_bits_right(x, 5, 4);
-    int z = get_bits_left(x, 3, 8);
-    // print_bits16(y);
+    int z = get_bits_left(x, 5, 4);
+    print_bits16(y);
+    print_bits16(z);
+    printf("----------------------------------------------\n");
 
-    unsigned int b = 0b1111111;
-    // print_bits16(b);
-    b = b >> 5;
-    // print_bits16(b);
+    printf("Discover Default Int length on Machine\n");
+    unsigned int word_length = word_length_calculator();
+    printf("WordLength of machine is: %d\n", word_length);
+    printf("----------------------------------------------\n");
+
+    printf("Rotate integer right\n");
+    unsigned short int right = 0b101010101010101;
+    print_bits16(right);
+    right = rotate_right(right,  3);
+    print_bits16(right);
+    unsigned short int right2 = 0b00000001010101;
+    print_bits16(right2);
+    right2 = rotate_right(right2,  4);
+    print_bits16(right2);
+    printf("----------------------------------------------\n");
 
     return 1;
 }
